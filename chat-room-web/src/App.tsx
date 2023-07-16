da164e4a6cbbf300cd7f5feab29b4f9abb400e6a
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-07-16 19:27:23 +0200

    ADD - Login cs Chat handling

--- chat-room-web/src/App.tsx
+++ chat-room-web/src/App.tsx
@@ -5,6 +5,10 @@ import { JoinForm } from "./components/JoinForm/JoinForm";
 function App() {
   const roomData = window.localStorage.getItem("room-data");
 
+  const logOutHandler = () => {
+    window.localStorage.removeItem("room-data");
+  };
+
   console.log(roomData);
   return (
     <div className="flex flex-col bg-gray-50-50">
@@ -12,13 +16,24 @@ function App() {
         {" "}
         Chat Rooms
       </h1>
-      <section className="flex flex-col border-1 w-fit pt-1 shadow-md rounded-sm border-gray-200 gap-4 self-center mt-24 ">
-        <h2 className="w-full p-4 text-center text-2xl bg-slate-400 text-gray-50 font-bold">
-          Join a room
-        </h2>
-        <JoinForm />
-      </section>
-      {roomData && <Chat />}
+      {roomData ? (
+        <div>
+          <Chat />
+          <button
+            onClick={logOutHandler}
+            className="w-1/3 mt-6 bg-red-400  p-2 rounded-lg text-gray-50 font-bold"
+          >
+            Log out
+          </button>
+        </div>
+      ) : (
+        <section className="flex flex-col border-1 w-fit pt-1 shadow-md rounded-sm border-gray-200 gap-4 self-center mt-24 ">
+          <h2 className="w-full p-4 text-center text-2xl bg-slate-400 text-gray-50 font-bold">
+            Join a room
+          </h2>
+          <JoinForm />
+        </section>
+      )}
     </div>
   );
 }
