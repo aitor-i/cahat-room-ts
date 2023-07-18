f20105ba28f16a10464a1f29bf4a648336491f29
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-07-18 20:52:39 +0200

    ADD(CCT) - Request view model

---
+++ chat-client-terminal/RequestVewModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chat_client_terminal
+{
+    public class RequestVewModel
+    {
+        public string username { get; set; }
+        public string roomName { get; set; }
+    }
+}
