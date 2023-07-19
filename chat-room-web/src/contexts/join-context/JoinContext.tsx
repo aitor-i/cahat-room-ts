6456ac4b31befa4d011fcd2d62d2f343b9b50293
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-07-19 17:46:58 +0200

    ADD(JoinContext) - Initial room messages

--- chat-room-web/src/contexts/join-context/JoinContext.tsx
+++ chat-room-web/src/contexts/join-context/JoinContext.tsx
@@ -1,11 +1,22 @@
 import { ReactNode, createContext, useState } from "react";
-import { RoomData } from "../../components/Chat/Chat";
+import { RoomData } from "../../components/Chat/useChat";
+
+export interface Client {
+  name: string;
+  id: string;
+}
+
+export interface Message {
+  client: Client;
+  message: string;
+}
 
 export const JoinContext = createContext<JoinContextInterface>({});
 
 export interface JoinContextInterface {
   setRoomData?: (data: RoomData) => void;
   roomData?: RoomData;
+  prevMessages?: Message[];
 }
 interface Props {
   children: ReactNode;
