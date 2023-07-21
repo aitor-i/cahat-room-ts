70a80d896b13b7d59752383269597810abfe46de
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-07-21 19:19:59 +0200

    ADD(chat-client-terminal) -  Fetch data Post "Abstraction"

--- chat-client-terminal/ApiConnection.cs
+++ chat-client-terminal/ApiConnection.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Text.Json;
+using RestSharp;
+
 
 namespace chat_client_terminal
 {
@@ -18,20 +20,49 @@ namespace chat_client_terminal
 		{
 			BaseUrl = baseUrl;
 			_HttpClient = new HttpClient();
-		}
+			
 
-		public async Task PostData (PostObject PostObject)
+        }
+
+		public async Task<HttpContent> PostData (PostObject PostObject)
 		{
+			HttpContent postDataResoponse;
 			string postEndpoint = BaseUrl + PostObject.Endpoint;
-			string jsonString = JsonSerializer.Serialize(postEndpoint);
+			string jsonString = JsonSerializer.Serialize(PostObject.DataToPost);
+
 
             var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
 			HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, postEndpoint);
             request.Headers.Add("Accept", "application/json");
             request.Content = content;
 
-            var res = await _HttpClient.SendAsync(request);
+            Task<HttpResponseMessage>  res =  _HttpClient.SendAsync(request);
+			Task.WaitAll(res);
+			await Task.WhenAny(res);
 
+			var response = await res;
+
+			if (!response.IsSuccessStatusCode) throw new Exception("In successfull post");
+			
+            postDataResoponse = response.Content;
+
+			return postDataResoponse;
+
+        }
+
+		public async Task FetchData (string endpoint)
+		{
+			string fetchUrl = BaseUrl + endpoint;
+
+            var client = new RestClient(fetchUrl);
+            var request = new RestRequest("", Method.Get);
+            Task<RestResponse> response =  client.ExecuteAsync(request);
+			Task.WaitAll(response);
+			await Task.WhenAny(response);
+            var content = await response;
+
+
+			Console.WriteLine(content.Content);
 
         }
 
